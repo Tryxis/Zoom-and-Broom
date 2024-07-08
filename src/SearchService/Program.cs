9edaed78902ad9d1140b423a02a24a250be36efa
Tryxis <47526388+Tryxis@users.noreply.github.com>
2024-07-08 10:09:05 +0100

    Added Consumer

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,6 +1,7 @@
 using MassTransit;
 using MongoDB.Driver;
 using MongoDB.Entities;
+using SearchService.Consumers;
 using SearchService.Data;
 using SearchService.Models;
 using SearchService.Services;
@@ -10,10 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddHttpClient<AuctionSvcHttpClient>();
 
 builder.Services.AddMassTransit( x=>
 {
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
     x.UsingRabbitMq((context, cfg) =>
     {
         cfg.ConfigureEndpoints(context);
