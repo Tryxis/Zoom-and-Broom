8e2e99620112079d04e516a49f36b82338ce1085
Tryxis <47526388+Tryxis@users.noreply.github.com>
2024-07-24 14:35:36 +0100

    IdentityDB seed and migration

--- src/IdentityService/SeedData.cs
+++ src/IdentityService/SeedData.cs
@@ -18,6 +18,13 @@ public class SeedData
             context.Database.Migrate();
 
             var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            // Exit if users exist
+            if(userMgr.Users.Any())
+            {
+                return;
+            }
+
             var alice = userMgr.FindByNameAsync("alice").Result;
             if (alice == null)
             {
@@ -35,9 +42,6 @@ public class SeedData
 
                 result = userMgr.AddClaimsAsync(alice, new Claim[]{
                             new Claim(JwtClaimTypes.Name, "Alice Smith"),
-                            new Claim(JwtClaimTypes.GivenName, "Alice"),
-                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
-                            new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
                         }).Result;
                 if (!result.Succeeded)
                 {
@@ -67,10 +71,6 @@ public class SeedData
 
                 result = userMgr.AddClaimsAsync(bob, new Claim[]{
                             new Claim(JwtClaimTypes.Name, "Bob Smith"),
-                            new Claim(JwtClaimTypes.GivenName, "Bob"),
-                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
-                            new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
-                            new Claim("location", "somewhere")
                         }).Result;
                 if (!result.Succeeded)
                 {
